d373ea8f42783717c1673d0328d21e0272ac79bf
Mahadev R Saravad <mahadevrsaravad@gmail.com>
2023-01-16 00:18:07 +0530

    Ans

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,5 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj =new RWOnly();
+        //obj.name="Mahadev";
+        obj.setName("Mahadev");
+        obj.getName();
     }
 }
